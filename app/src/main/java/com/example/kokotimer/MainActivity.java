9f5c277dd5bc6a49159999cea283ac3fca293a9e
Mavorr <mkasperski0@gmail.com>
2019-11-05 13:37:43 +0100

    commit changes

--- app/src/main/java/com/example/kokotimer/MainActivity.java
+++ app/src/main/java/com/example/kokotimer/MainActivity.java
@@ -1,33 +1,72 @@
 package com.example.kokotimer;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView TimerTextView;
+    SeekBar TimerSeekBar;
+    Boolean counterIsActive = false;
+    Button goButton;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer() {
+
+        TimerTextView.setText("0:30");
+        TimerSeekBar.setProgress(30);
+        TimerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        goButton.setText("KNEFEL");
+        counterIsActive = false;
+    }
 
     public void buttonClicked(View view){
 
-        CountDownTimer countDownTimer = new CountDownTimer(10000, 1000) {
+        if  (counterIsActive) {
+            resetTimer();
 
-            @Override
-            public void onTick(long millisUntilFinished) {
 
-            }
+        } else {
+            counterIsActive = true;
+            TimerSeekBar.setEnabled(false);
+            goButton.setText("STOP");
 
-            @Override
-            public void onFinish() {
+            countDownTimer = new CountDownTimer(TimerSeekBar.getProgress() * 1000 + 100, 1000) {
 
-            }
-        }.start();
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.note1_c);
+                    mplayer.start();
+                    resetTimer();
+                }
+            }.start();
+        }
 
     }
 
     public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+        TimerTextView.setText(Integer.toString(minutes) + ":" + secondString);
 
     }
 
@@ -37,8 +76,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        SeekBar TimerSeekBar = findViewById(R.id.TimerSeekBar);
-        final TextView TimerTextView = findViewById(R.id.CountDownTextView);
+        TimerSeekBar = findViewById(R.id.TimerSeekBar);
+        TimerTextView = findViewById(R.id.CountDownTextView);
+        goButton = findViewById(R.id.Button);
 
         TimerSeekBar.setMax(600);
         TimerSeekBar.setProgress(30);
@@ -46,15 +86,8 @@ public class MainActivity extends AppCompatActivity {
         TimerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minutes = progress / 60;
-                int seconds = progress - (minutes * 60);
+                updateTimer(progress);
 
-                String secondString = Integer.toString(seconds);
-
-                if (secondString.equals("0")){
-                    secondString = "00";
-                }
-                TimerTextView.setText(Integer.toString(minutes) + ":" + secondString);
 
             }
 
